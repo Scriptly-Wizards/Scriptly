7f8665593c42906746ec7437c4e93fc5f6d091d7
Mario <liang.chen829@gmail.com>
2024-06-30 09:18:51 -0700

    feat: extract music from script

--- backend/assistant/views.py
+++ backend/assistant/views.py
@@ -5,12 +5,16 @@ from rest_framework import status
 from .serializers import VideoDataSerializer
 from .services import OpenAIService
 
+services = {}
+
 
 class VideoDataView(APIView):
     def post(self, request):
         serializer = VideoDataSerializer(data=request.data)
         if serializer.is_valid():
+            token = request.headers.get('Authorization', 'default')
             assistant_service = OpenAIService()
+            services[token] = assistant_service
             try:
                 response_data = assistant_service.process_request(
                     serializer.generate_content())
@@ -19,3 +23,19 @@ class VideoDataView(APIView):
                 # Log the exception e
                 return Response({"error": "Failed to process request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MusicDataView(APIView):
+    def post(self, request):
+        token = request.headers.get('Authorization', 'default')
+        try:
+            assistant_service = services[token]
+        except KeyError:
+            return Response({'message': 'assistant not found'}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            prompt = "Give me the most appropriate musical style and theme for the script you provided. Use genres and vibes, not specific artists and songs. Please be brief and concise."
+            response_data = assistant_service.process_request(prompt)
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            # Log the exception e
+            return Response({"error": "Failed to process request"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
