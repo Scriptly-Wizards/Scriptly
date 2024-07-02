38986745e113283fa1649d419789690c979746a7
coulson-tu <156402192+coulson-tu@users.noreply.github.com>
2024-07-02 11:51:51 -0700

    add ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, {useRef, useState, useEffect} from "react";
+"use client";
+import React, { useRef, useState, useEffect } from "react";
 import '../styles/globals.css';
 import Form from "@/component/Form/Form";
 
