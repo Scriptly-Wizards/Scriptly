8d934e8570b73153de84004adf314182ac3565fa
Mario <liang.chen829@gmail.com>
2024-07-02 14:03:01 -0700

    feat: Add react-markdown dependency and implement preview rendering

--- frontend/src/component/Page/Preview.tsx
+++ frontend/src/component/Page/Preview.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { CustomMessage } from '../../store/message/message-slice';
+import ReactMarkdown from 'react-markdown';
+import './Preview.css';
 
 const Preview = () => {
-  const dataFromStore = useSelector((state: RootState) => state.message.message);
+  const dataFromStore = useSelector(
+    (state: RootState) => state.message.message
+  );
   const [data, setData] = useState<CustomMessage | null>(null);
 
   useEffect(() => {
-    console.log('useEffect data', data)
-    console.log('useEffect dataFromStore', dataFromStore)
+    console.log('useEffect data', data);
+    console.log('useEffect dataFromStore', dataFromStore);
     setData(dataFromStore);
   }, [data, dataFromStore]);
 
@@ -17,14 +21,14 @@ const Preview = () => {
     <div>
       <h1>Preview Page</h1>
       {data ? (
-        <div>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div className="markdown-container">
+          <ReactMarkdown>{data.value}</ReactMarkdown>
         </div>
       ) : (
         <p>No data to display</p>
       )}
     </div>
   );
-}
+};
 
-export default Preview
+export default Preview;
