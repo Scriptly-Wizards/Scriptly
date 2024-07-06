4d7f0c2fa3736cc34cdfd8040749956cd06cb459
Mario <liang.chen829@gmail.com>
2024-07-06 15:54:01 -0700

    fix: UI update

--- frontend/src/component/Page/Preview.tsx
+++ frontend/src/component/Page/Preview.tsx
@@ -1,20 +1,18 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import { CustomMessage } from '../../store/message/messageSlice';
-import { useAppDispatch } from '../../hooks';
-import { getPdfFile } from '../../store/pdf/pdfService';
-import ReactMarkdown from 'react-markdown';
-import './Preview.css';
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { CustomMessage } from "../../store/message/messageSlice";
+import { useAppDispatch } from "../../hooks";
+import { getPdfFile } from "../../store/pdf/pdfService";
+import ReactMarkdown from "react-markdown";
+import "./Preview.css";
 
 const Preview = () => {
   const dispatch = useAppDispatch();
   const dataFromStore = useSelector(
     (state: RootState) => state.message.message
   );
-  const {pdfUrl, error} = useSelector(
-    (state: RootState) => state.pdfFile
-  );
+  const { pdfUrl, error } = useSelector((state: RootState) => state.pdfFile);
   const [data, setData] = useState<CustomMessage | null>(null);
 
   useEffect(() => {
@@ -28,18 +26,23 @@ const Preview = () => {
     <div>
       <h1>Preview Page</h1>
       {data ? (
-        <>
+        <div className="preview-container">
           <div className="markdown-container">
             <ReactMarkdown>{data.value}</ReactMarkdown>
           </div>
-          <div className="flex justify-center items-center mt-4">
-            <button onClick={() => pdfUrl && window.open(pdfUrl)} 
-              className={`${error ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'
-              } text-white font-bold py-2 px-4 rounded flex items-center`} disabled={error}>
+          <div className="download-btn">
+            <button
+              onClick={() => pdfUrl && window.open(pdfUrl)}
+              className={`${
+                error
+                  ? "bg-gray-400 cursor-not-allowed"
+                  : "bg-blue-500 hover:bg-blue-700"
+              } text-white font-bold py-2 px-4 rounded flex items-center`}
+              disabled={error}>
               Download PDF
             </button>
           </div>
-        </>
+        </div>
       ) : (
         <p>No data to display</p>
       )}
--- frontend/src/component/Page/Welcome.tsx
+++ frontend/src/component/Page/Welcome.tsx
@@ -1,49 +1,31 @@
-import { useEffect, useRef, useState } from "react";
-import Form from "../UI/Form/Form";
 import "./Welcome.css";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  const formRef = useRef<HTMLDivElement>(null);
-  const [showForm, setShowForm] = useState(false);
-
+  const navigate = useNavigate();
   const scrollToForm = () => {
-    setShowForm(true);
+    navigate("/form");
   };
 
-  useEffect(() => {
-    if (showForm && formRef.current) {
-      formRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [showForm]);
-
   return (
-    <div>
-      <div className="landing-page">
-        <img
-          src={`${process.env.PUBLIC_URL}/img/elisabeth-pieringer-9paY25EHOBo-unsplash.gif`}
-          alt="Welcome"
-          className="welcome-image"
-        />
-        <h1 className="welcome-title">
-          Welcome to Your <br />
-          Viral TikTok Script Generator
-        </h1>
-        <p className="welcome-description">
-          <span>Scriptly</span> is a recipe-style generator designed for TikTok
-          for TikTok users. It turns user prompts into GenAI inputs to create
-          shooting guides, scripts, suggestions, music, and samples.
-        </p>
-        <button onClick={scrollToForm} className="get-script-btn">
-          GET MY SCRIPT
-        </button>
-      </div>
-      <div style={{ marginBottom: "10rem" }}>
-        {showForm && (
-          <div ref={formRef}>
-            <Form />
-          </div>
-        )}
-      </div>
+    <div className="landing-page">
+      <img
+        src={`${process.env.PUBLIC_URL}/img/elisabeth-pieringer-9paY25EHOBo-unsplash.gif`}
+        alt="Welcome"
+        className="welcome-image"
+      />
+      <h1 className="welcome-title">
+        Welcome to Your <br />
+        Viral TikTok Script Generator
+      </h1>
+      <p className="welcome-description">
+        <span>Scriptly</span> is a recipe-style generator designed for TikTok
+        for TikTok users. It turns user prompts into GenAI inputs to create
+        shooting guides, scripts, suggestions, music, and samples.
+      </p>
+      <button onClick={scrollToForm} className="get-script-btn">
+        GET MY SCRIPT
+      </button>
     </div>
   );
 };
