95511e983b374e7e61c48e4e4a8038e3fd3b2ba3
Lena Zhang <lena.zhang@tpisoftware.com>
2024-07-08 15:55:09 -0700

    update: backend service URL

--- frontend/src/Environment.ts
+++ frontend/src/Environment.ts
@@ -1,6 +1,6 @@
 
 const ENVIRONMENT = {
-  apiUrl: 'https://scriptly-105d4cc5ed46.herokuapp.com/api/v1'
+  apiUrl: 'scriptly-wizard-7c14c9048571.herokuapp.com/api/v1'
 };
 
 export default ENVIRONMENT;
